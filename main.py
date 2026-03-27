f20c733e7cd92d6318b44c691fb61fc37508bbac
Nozila <nozilarajabboyeva@gmail.com>
2026-03-27 17:23:43 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# 16
+f16 = lambda a, b: (a + b) > 50
+
+# 17
+f17 = lambda x: "Good" if x > 0 and x % 2 == 0 else "Bad"
+
+# 18
+f18 = lambda x: x**2 if x % 4 == 0 else x**3
+
+# 19
+f19 = lambda x: 10 if x < 10 else x
+
+# 20
+f20 = lambda a, b: a if a == b else (a + b) / 2
